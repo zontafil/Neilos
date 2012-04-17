d670b3384d12b22794ac39541fbe44ce11294272
Filippo Zonta <kknull0@gmail.com>
2012-04-17 18:26:36 +0200

    Added missing files - 2

---
+++ vars.php
@@ -0,0 +1,12 @@
+<?php
+	$_SITENAME = 'neilos';
+	
+	$_VAR['version'] = "1.2.2";
+
+	$_ANALYTICS = FALSE;
+	$_ANALYTICS_ID = 'UA-29416196-1';
+	$_STATCOUNTER = FALSE;
+	$_STATCOUNTER_PROJ = '7692990';
+	$_STATCOUNTER_SEC = 'a9de00c5';
+
+?>
