0b6121c56dd25de08420a281683dc0ff61b17e43
m3l7 <kknull0@gmail.com>
2012-08-25 22:22:01 +0200

    PHP Core: Delete xml tag container

--- resources/load.php
+++ resources/load.php
@@ -4,6 +4,6 @@
 // can decide to use file or sql storage
 
 include_once 'lib/lib_content_loader.php';
-echo "<xml>".load_content($_GET)."</xml>";
-
+//echo "<xml>".load_content($_GET)."</xml>";
+echo load_content($_GET);
 ?>
